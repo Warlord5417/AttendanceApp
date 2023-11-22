7c9bb40f248c9bfad4de5f32eff5a7ca6e30f1df
Jaypee <jaypee.zulieta@lsu.edu.ph>
2023-11-22 11:33:25 +0800

    feat: Add initial implementation of the roleRoute

---
+++ src/Role/routes/RoleRoute.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+import { RoleRepoPrismaImpl } from "../repo/RoleRepoPrismaImpl";
+import { RoleServiceImpl } from "../services/RoleServiceImpl";
+import { RoleController } from "./RoleController";
+import { Context } from "hono";
+import { RoleDoesNotExistError } from "../error/RoleDoesNotExistError";
+
+export const roleRoute = new RoleController(
+    new RoleServiceImpl(
+        new RoleRepoPrismaImpl(
+            new PrismaClient()
+        )
+    )
+);
+
+const service = roleRoute.getService()
+
+roleRoute.post('/create', async (c: Context) => {
+    try{
+        const requestBody = await c.req.json()
+        const role = await service.createRole(requestBody)
+        return c.json(role)
+    }
+    catch(err){
+        if(err instanceof RoleDoesNotExistError){
+            c.status(400)
+            return c.json({ message: err.message })
+        }
+        c.status(500)
+        return c.json({ message: 'Server Error'})
+    }
+})
