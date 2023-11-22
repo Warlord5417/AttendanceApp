00360920677de7f20b8aa87133bdb7fed738fcda
Jaypee <jaypee.zulieta@lsu.edu.ph>
2023-11-22 11:57:48 +0800

    feat: Expose the roleRoute

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { roleRoute } from './Role/routes/RoleRoute'
 
 const app = new Hono()
-app.get('/', (c) => c.text('Hello Hono!'))
+app.route('/role', roleRoute)
 
 serve(app)
