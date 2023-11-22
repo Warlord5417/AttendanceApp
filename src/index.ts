e94287b10e270966b91038f1db17eceb56b7f22a
Jaypee <jaypee.zulieta@lsu.edu.ph>
2023-11-22 12:49:44 +0800

    feat: Add CORS

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,17 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { roleRoute } from './Role/routes/RoleRoute'
 import { font } from 'ascii-art'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
+app.use('/*', cors({
+    origin: 'http://localhost:5173',
+    allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests'],
+    allowMethods: ['POST', 'GET', 'PUT', 'DELETE'],
+    exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+    maxAge: 600,
+    credentials: true,
+}))
 app.route('/role', roleRoute)
 
 serve(app, async () => {
