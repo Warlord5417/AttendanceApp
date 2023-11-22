1f7561c4d876edb66a488ca3e561000d2e5d3c1f
Jaypee <jaypee.zulieta@lsu.edu.ph>
2023-11-22 12:19:20 +0800

    feat: Add ascii-art on startup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { roleRoute } from './Role/routes/RoleRoute'
+import { font } from 'ascii-art'
 
 const app = new Hono()
 app.route('/role', roleRoute)
 
-serve(app)
+serve(app, async () => {
+    const title = await font('Attendance', 'Doom').toPromise()
+    console.log(title)
+    console.log('Server started on port 3000')
+})
