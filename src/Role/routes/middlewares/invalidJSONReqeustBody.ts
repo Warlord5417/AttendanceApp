0e95aaca007c8d66b9ebc19c9950ba8fc3fd0e67
Jaypee <jaypee.zulieta@lsu.edu.ph>
2023-11-22 12:23:51 +0800

    feat: add invalidJSONRequestBodyFilter

---
+++ src/Role/routes/middlewares/invalidJSONReqeustBody.ts
@@ -0,0 +1,12 @@
+import { Context } from "hono";
+
+export async function invalidJSONRequestBody(c: Context, next: Function){
+    try{
+        await c.req.json()
+        await next()
+    }
+    catch(err){
+        c.status(400)
+        return c.json({ message: 'Invalid JSON request body'})
+    }
+}
